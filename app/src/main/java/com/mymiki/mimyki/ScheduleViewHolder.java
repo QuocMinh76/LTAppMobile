543336593a3e1db9551df85ab9c0afbb09b40eb5
LTYenMy246 <leyenmy104@gmail.com>
2024-12-04 00:33:34 +0700

    my giao dien schedule

---
+++ app/src/main/java/com/mymiki/mimyki/ScheduleViewHolder.java
@@ -0,0 +1,13 @@
+package com.mymiki.mimyki;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ScheduleViewHolder extends RecyclerView.ViewHolder{
+    public ScheduleViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
