064d65496da6aaefa5017078e5fe0b50e415f032
trakie <trantuankiet.authentic@gmail.com>
2024-12-06 23:59:46 +0700

    Kiet Create

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.google.android.material:material:1.7.0")
 }
