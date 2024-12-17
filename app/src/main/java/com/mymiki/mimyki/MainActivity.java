8f696b46bd57e3c5c5b80d969c2e1f07d89397d0
trakie <93091344+trakie@users.noreply.github.com>
2024-12-18 01:17:38 +0700

    Kiet Upd

--- app/src/main/java/com/mymiki/mimyki/MainActivity.java
+++ app/src/main/java/com/mymiki/mimyki/MainActivity.java
@@ -1,8 +1,24 @@
 package com.mymiki.mimyki;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.bottom_nav_view, new TaskFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.nav_tasks) {
+                        selectedFragment = new TaskFragment();
+                    } else if (item.getItemId() == R.id.nav_calendar) {
+                        selectedFragment = new CalendarFragment();
+                    }else if (item.getItemId() == R.id.nav_matrix) {
+                        selectedFragment = new MatrixFragment();
+                    } else if (item.getItemId() == R.id.nav_setting) {
+                        selectedFragment = new SettingFragment();
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_nav_view, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
